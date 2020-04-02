70ed5ffc5ef4d8a08c16af9b48cc1cade64170ed
22-Pig <845501558@qq.com>
2020-04-02 22:48:17 +0800

    整合

---
+++ syd1802/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Article } from './article';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  articles: Article[]; // 增加
+
+  // 该函数新增
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    // 增加下面两行代码
+    const obj = new Article(title.value, link.value, 0);
+    this.articles.push(obj);
+    return false;
+  }
+}
